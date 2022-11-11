2f66f47369aca121b1566b2d013e4ab508443f16
jalig <117361652+jalig@users.noreply.github.com>
2022-11-11 23:51:58 +0400

    Add files via upload

---
+++ Leason/src/Main.java
@@ -0,0 +1,61 @@
+public class Main {
+    public static void main(String[] args) {
+
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend +2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        var weightFirstFighter = 78.2;
+        var weightSecondFighter = 82.7;
+        var totalWeight = weightFirstFighter + weightSecondFighter;
+        var difference = weightSecondFighter - weightFirstFighter;
+        System.out.println("Общий вес бойцов " + totalWeight + " кг");
+        System.out.println("Разница в весе " + difference + " кг");
+        difference = weightSecondFighter - weightFirstFighter; // Фактически добавленна для соблюдения условий задачи (или я не понял)
+        var odds = weightSecondFighter % weightFirstFighter;
+        System.out.println("Приемущество в весе " + odds + " кг");
+
+        var allTime = 640;
+        var onePersonTime = 8;
+        var personCount = allTime / onePersonTime;
+        System.out.println("Всего работников в компании —  " + personCount + " человек");
+        var morePersons = personCount + 94;
+        System.out.println("Если в компании работает " + morePersons + " человека, то всего " + morePersons * onePersonTime + " часов работы может быть поделено между сотрудниками");
+
+
+
+    }
+}
